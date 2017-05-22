affd3878701e91f788a94d1fa2da49032840a487
Philippe Luickx <philippe.luickx@gmail.com>
2017-05-22 17:07:30 +0300

    more fix

--- apinf_bot/apis/restviews.py
+++ apinf_bot/apis/restviews.py
@@ -151,13 +151,14 @@ class BotView(APIView):
 
                     # This is ugly - no way out to get a serializer field name
                     # with a dot in the name
-                    url = request.data['result']['contexts'][0]['parameters']['url.original']
-                    if url_is_alive(parameters['url']):
-                        url = parameters['url']
-                    elif url_is_alive('http://' + parameters['url']):
-                        url = 'http://' + parameters['url']
-                    elif url_is_alive('https://' + parameters['url']):
-                        url = 'https://' + parameters['url']
+                    request_url = request.data['result']['contexts'][0]['parameters']['url.original']
+                    if url_is_alive(request_url):
+                        url = request_url
+                    elif url_is_alive('https://' + request_url):
+                        url = 'https://' + request_url
+                    elif url_is_alive('http://' + request_url):
+                        url = 'http://' + request_url
+
                     else:
                         output_data['displayText'] = _('{0} - This is an invalid URL!').format(parameters['url'])
 
