9e5c392f3d759eca31af6969fe8b0484b38fd2ce
Mauricio Vieira <mauricio@mauriciovieira.net>
2017-08-01 10:35:44 -0300

    Uncomment boilerplate code

--- config/urls.py
+++ config/urls.py
@@ -3,20 +3,21 @@ from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.views import defaults as default_views
+from django.views.generic import TemplateView
 api_patterns = [
     url(r'^apis/', include('apibot.apis.urls')),
 
 ]
 urlpatterns = [
-    #   url(r'^$', TemplateView.as_view(template_name='pages/home.html'), name='home'),
-    #   url(r'^about/$', TemplateView.as_view(template_name='pages/about.html'), name='about'),
+    url(r'^$', TemplateView.as_view(template_name='pages/home.html'), name='home'),
+    url(r'^about/$', TemplateView.as_view(template_name='pages/about.html'), name='about'),
 
     # Django Admin, use {% url 'admin:index' %}
     url(settings.ADMIN_URL, admin.site.urls),
 
     # User management
     url(r'^users/', include('apibot.users.urls', namespace='users')),
-    # url(r'^accounts/', include('allauth.urls')),
+    url(r'^accounts/', include('allauth.urls')),
 
     # Your stuff: custom urls includes go here
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
